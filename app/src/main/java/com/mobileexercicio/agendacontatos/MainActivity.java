d9e96c8d449ef9825f4411f140c8ddc3b82b74ed
magela-lopez <71371549+magela-lopez@users.noreply.github.com>
2022-04-07 20:12:19 -0300

    Search View

--- app/src/main/java/com/mobileexercicio/agendacontatos/MainActivity.java
+++ app/src/main/java/com/mobileexercicio/agendacontatos/MainActivity.java
@@ -1,14 +1,18 @@
 package com.mobileexercicio.agendacontatos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -58,6 +62,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main,menu);
+
+        MenuItem menuItemSearch = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItemSearch.getActionView();
+        searchView.setOnQueryTextListener(onSearch());
+
         return true;
+    }
+
+    private SearchView.OnQueryTextListener onSearch(){
+        return new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(TextUtils.isEmpty(newText)){
+                    contatos.clearTextFilter();
+                }else{
+                    contatos.setFilterText(newText);
+                    //contatos = newText;
+
+                }
+                return false;
+            }
+        };
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.action_search){
+
+
+        }else if(item.getItemId() == R.id.action_add){
+            Intent intent = new Intent(getApplicationContext(), AgregarActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
