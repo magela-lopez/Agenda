993793115d6e0bf2f2231553ffb7c1ebbc24861f
magela-lopez <71371549+magela-lopez@users.noreply.github.com>
2022-04-07 17:14:22 -0300

    Add ListView

--- app/src/main/java/com/mobileexercicio/agendacontatos/MainActivity.java
+++ app/src/main/java/com/mobileexercicio/agendacontatos/MainActivity.java
@@ -6,16 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView contatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contatos = findViewById(R.id.listContatos);
+        String listContacts []= {"Magela","Carmen","Cirila","Gimena"};
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listContacts);
+        contatos.setAdapter(adapter);
+
+        contatos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String nameContact = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), nameContact, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         FloatingActionButton floatingActionButton = findViewById(R.id.floatingBtnAdd);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
